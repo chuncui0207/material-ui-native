758c93dcf649e99bc0305dd0955ac760af16a585
Nathaniel Maman <maman.nathaniel@gmail.com>
2015-12-03 16:37:59 +0200

    rm style-propable imports

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,6 +1,5 @@
 export default {
   ClickAwayable: require('./click-awayable'),
   WindowListenable: require('./window-listenable'),
-  StylePropable: require('./style-propable'),
   StyleResizable: require('./style-resizable'),
 };
